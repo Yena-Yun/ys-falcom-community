7d5bb44085bb00a1c84689830f5b3a0d80e04658
Yena-Yun <cheryleduart@gmail.com>
2021-10-31 18:12:30 +0900

    favicon 및 웹 타이틀 수정

--- ys-falcom-lounge/src/App.js
+++ ys-falcom-lounge/src/App.js
@@ -1,34 +1,35 @@
-import { useState } from 'react';
-import ReactHowler from 'react-howler';
+import { Suspense, lazy } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Background from './components/Background';
 import MainEntry from './pages/MainEntry';
-import Home from './pages/Home';
-import History from './pages/History';
-import Gallery from './pages/Gallery';
-import Tournament from './pages/Tournament';
-import Music from './pages/Music';
-import ImageDetail from './pages/ImageDetail';
 import PageNotFound from './pages/PageNotFound';
+
+const Home = lazy(() => import('./pages/Home'));
+const History = lazy(() => import('./pages/History'));
+const Gallery = lazy(() => import('./pages/Gallery'));
+const ImageDetail = lazy(() => import('./pages/ImageDetail'));
+const Tournament = lazy(() => import('./pages/Tournament'));
+const Music = lazy(() => import('./pages/Music'));
 
 function App() {
   return (
     <>
-      {/* <ReactHowler src={BackgroundMusic} playing={false} /> */}
-      <Route path='/' exact component={MainEntry} />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Route path='/' exact component={MainEntry} />
 
-      <Background>
-        <Switch>
-          <Route path='/home' component={Home} />
-          <Route path='/history' component={History} />
-          <Route path='/gallery' exact component={Gallery} />
-          <Route path='/gallery/:selected/:id' component={ImageDetail} />
-          <Route path='/worldcup' component={Tournament} />
-          <Route path='/music' component={Music} />
-          {/* 어떠한 URL로도 이동할 수 없을 때 띄울 에러페이지 */}
-          <Route component={PageNotFound} />
-        </Switch>
-      </Background>
+        <Background>
+          <Switch>
+            <Route path='/home' component={Home} />
+            <Route path='/history' component={History} />
+            <Route path='/gallery' exact component={Gallery} />
+            <Route path='/gallery/:selected/:id' component={ImageDetail} />
+            <Route path='/worldcup' component={Tournament} />
+            <Route path='/music' component={Music} />
+            {/* 어떠한 URL로도 이동할 수 없을 때 띄울 에러페이지 */}
+            <Route component={PageNotFound} />
+          </Switch>
+        </Background>
+      </Suspense>
     </>
   );
 }
